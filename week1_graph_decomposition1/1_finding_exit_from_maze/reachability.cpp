980b71a313255f23a0690c83dae6b9b199294772
TomLan42 <lhc980616@gmail.com>
2021-04-24 23:18:47 +0800

    week 1 assignment

--- week1_graph_decomposition1/1_finding_exit_from_maze/reachability.cpp
+++ week1_graph_decomposition1/1_finding_exit_from_maze/reachability.cpp
@@ -5,20 +5,37 @@ using std::vector;
 using std::pair;
 
 
-int reach(vector<vector<int> > &adj, int x, int y) {
-  //write your code here
-  return 0;
+// adj is the adj list
+// x is the starting vertice
+// y is the target vertice
+int reach(vector<pair<vector<int>, bool > > &adj, int x, int y) {
+  
+  if (x == y) {
+    return 1;
+  }
+
+  // visit
+  adj[x].second = true;
+  
+  int result = 0;
+  for (int i = 0; i < adj[x].first.size(); ++i) {
+    if (!adj[adj[x].first[i]].second) {
+      // dfs
+      result = result | reach(adj, adj[x].first[i], y);
+    }
+  }
+  return result;
 }
 
 int main() {
   size_t n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
+  vector<pair<vector<int>, bool > > adj(n, std::make_pair(vector<int>(), false));
   for (size_t i = 0; i < m; i++) {
     int x, y;
     std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
+    adj[x - 1].first.push_back(y - 1);
+    adj[y - 1].first.push_back(x - 1);;
   }
   int x, y;
   std::cin >> x >> y;
--- week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.cpp
+++ week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.cpp
@@ -4,21 +4,41 @@
 using std::vector;
 using std::pair;
 
-int number_of_components(vector<vector<int> > &adj) {
+
+void reach(vector<pair<vector<int>, bool > > &adj, int x) {
+
+  // visit
+  adj[x].second = true;
+  for (int i = 0; i < adj[x].first.size(); ++i) {
+    if (!adj[adj[x].first[i]].second) {
+      reach(adj, adj[x].first[i]);
+    }
+  }
+}
+
+
+
+int number_of_components(vector<pair<vector<int>, bool > > &adj) {
+  
   int res = 0;
-  //write your code here
+  for (int i = 0; i < adj.size(); ++i) {
+    if (!adj[i].second) {
+      res++;
+      reach(adj,i);
+    }
+  }
   return res;
 }
 
 int main() {
   size_t n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
+  vector<pair<vector<int>, bool > > adj(n, std::make_pair(vector<int>(), false));
   for (size_t i = 0; i < m; i++) {
     int x, y;
     std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
+    adj[x - 1].first.push_back(y - 1);
+    adj[y - 1].first.push_back(x - 1);;
   }
   std::cout << number_of_components(adj);
 }
